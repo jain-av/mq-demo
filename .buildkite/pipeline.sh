240fb17bf7ce043938c2c5bc4138357effad76a6
Ankit Jain <ankit@aviator.co>
2025-12-01 13:34:46 -0800

    use relativep ath

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -10,5 +10,5 @@ test_files=$(find . -maxdepth 1 -name "test_*.py" -type f | sort)
 
 # add a new command step to run the tests in each test directory
 for test_file in $test_files; do
-  echo "  - command: \"pytest_single.sh "${test_file}"\""
+  echo "  - command: \"./pytest_single.sh "${test_file}"\""
 done
